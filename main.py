4d48dd33797700a0f53ff81066eb38143a8548a4
VersyLence <megivan25@gmail.com>
2025-12-09 23:56:26 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+from datetime import date
+
+class get_holiday:
+    def __init__(self):
+        self.today = date.today()
+        self.year = self.today.year
+        self.month = self.today.month
+        self.day = self.today.day
+        self.API_KEY = ''
+        self.url = 'https://calendarific.com/api/v2/holidays'
+        self.country_code = 'RU'
+        self.params = {
+            'api_key': self.API_KEY,
+            'country': 'RU',
+            'year': self.year,
+            'month': self.month,
+            'day': self.day
+        }
+
+    def holiday_today(self):
+        response = requests.get(self.url, params=self.params)
+        data = response.json()
+
+        if 'response' in data and 'holidays' in data['response']:
+            holidays = data['response']['holidays']
+            if holidays:
+                print(f"Сегодня ({self.today}) отмечаются следующие праздники:")
+                for holiday in holidays:
+                    print(f"- {holiday['name']}")
+            else:
+                print(f"Сегодня ({self.today}) праздников не найдено.")
+
+
+
+test = get_holiday()
+test.holiday_today()
